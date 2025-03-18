a7edf73dea925edb395ef227f1f9df9810c363fc
Hansadhwaja Biswal <hansadhwaja.biswal@pixelcompute.com>
2025-03-18 13:34:42 +0530

    Added git hooks, eslint, prettier and rubocop

--- db/migrate/20250313074817_create_posts.rb
+++ db/migrate/20250313074817_create_posts.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
       t.string :title, null: false
       t.text :description, null: false
-      t.integer :upvote,null: false, default: 0
-      t.integer :downvote,null: false, default: 0
+      t.integer :upvote, null: false, default: 0
+      t.integer :downvote, null: false, default: 0
       t.boolean :is_bloggable, default: false
 
       t.timestamps
